133fe029c5555d2671e05946453188db91d35b1f
orlo <oguerrerofarias@gmail.com>
2021-06-28 21:01:43 -0400

    adding http requests

---
+++ ProtoTinkr/Managers/LoginManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace ProtoTinkr
+{
+    public static class LoginManager
+    {
+        public static Task Login()
+        {
+            
+        }
+    }
+}
--- ProtoTinkr/Program.cs
+++ ProtoTinkr/Program.cs
@@ -12,12 +12,15 @@ namespace ProtoTinkr
 {
     public class Program
     {
+        public static PlayerProfile _profile { get; set; }
+        public DateTime LoginTime { get; set; }
         public static async Task Main(string[] args)
         {
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NDYzNzczQDMxMzkyZTMxMmUzMGw4VDBTVDRKRTdyVzFiUW9LNllCVWZoL2x0ckxqMjJISUZHa2g5TFgxZU09;NDYzNzc0QDMxMzkyZTMxMmUzMEhGdVI3dlhyc1V4dkdIL0pjZ2FvZjNBaVFHV0hBUVZEbG4zRGRlNFBFZ1k9;NDYzNzc1QDMxMzkyZTMxMmUzMGFTWWN4N1BnM1lRZkFSdHZFY0xvRXprZWpnaFJaQXIzVUZFL25ZeFRZT009;NDYzNzc2QDMxMzkyZTMxMmUzMEZVYkl3N3hKRXB5L0llTVdhaVVxUDQvbjBhVXRlR2pGczZGcTJwU0tNWEE9");
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.Services.AddSyncfusionBlazor();
+            builder.Services.AddSingleton<HttpClient>();
             builder.Services.AddScoped(
                 sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
             await builder.Build().RunAsync();
